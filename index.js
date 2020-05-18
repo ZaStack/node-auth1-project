54b1edc2faa49e817ee1e6afc3ac25c19d1298f8
ZaStack <stackzpersonal@gmail.com>
2020-05-18 12:28:41 -0700

    Builds index.js and server.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-requ
+require('dotenv').config()
+const server = require('./api/server')
+
+const PORT  = process.env.PORT || 5000
+server.listen(PORT, () => console.log(`Server running on ${PORT}`))
